36ffdc1eab830e52788c8f8e2e932e5e13959226
sung-ik-je <ij16_21@naver.com>
2023-02-08 15:34:48 +0900

    1단계

---
+++ src/main/java/com/example/demo/TtestApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtestApplication.class, args);
+	}
+
+}
