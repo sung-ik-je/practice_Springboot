a7022dc81da9e38e24888c677fbbd316a54e3d0f
sung-ik-je <ij16_21@naver.com>
2023-02-08 16:47:38 +0900

    html 연동 및 thymeleaf 확인

---
+++ html0/README.md
@@ -0,0 +1,4 @@
+# practice
+
+
+# html 태그 관련 연습 목적
---
+++ html0/src/main/java/com/example/demo/Html0Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Html0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Html0Application.class, args);
+	}
+
+}
