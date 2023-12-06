13f7ccfaecfead3e200ea9822492b9bd0598d8a5
sung-ik-je <ij16_21@naver.com>
2023-12-06 20:55:36 +0900

    di, ioc 적용 초기 세팅

---
+++ springweb-ch02_2/src/main/java/org/mycompany/ordersystem/AppConfig.java
@@ -0,0 +1,9 @@
+package org.mycompany.ordersystem;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {"com.mycompany.ordersystem"})
+public class AppConfig {
+}
---
+++ springweb-ch02_2/src/main/java/org/mycompany/ordersystem/Main.java
@@ -0,0 +1,11 @@
+package org.mycompany.ordersystem;
+
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+    }
+}
