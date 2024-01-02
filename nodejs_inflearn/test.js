c90783cb7d4d43209a3ac054bbbb66b9f2411f6f
sung-ik-je <ij16_21@naver.com>
2024-01-02 15:22:05 +0900

    ghost 실습

--- nodejs_inflearn/test.js
+++ nodejs_inflearn/test.js
@@ -1,1 +1,18 @@
-hello
+function oneMore() {
+    console.log('one more')
+}
+
+function run() {
+    console.log('run run');
+    setTimeout(() => {
+        console.log('wow');
+    }, 0)
+
+    new Promise((resolve) => {
+        resolve('hi');
+    })
+    .then(console.log);
+    oneMore()
+}
+
+setTimeout(run, 5000);
