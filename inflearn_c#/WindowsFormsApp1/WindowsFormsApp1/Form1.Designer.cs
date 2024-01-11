1f4211174b8ed8f3b6d005f4bc284f4d8bceec69
sung-ik-je <ij16_21@naver.com>
2024-01-11 16:26:39 +0900

    로그인 폼

--- inflearn_c#/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ inflearn_c#/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -57,9 +57,9 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("굴림", 14F);
-            this.label1.Location = new System.Drawing.Point(342, 278);
+            this.label1.Location = new System.Drawing.Point(301, 282);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(86, 30);
+            this.label1.Size = new System.Drawing.Size(69, 24);
             this.label1.TabIndex = 2;
             this.label1.Text = "label1";
             this.label1.Click += new System.EventHandler(this.label1_Click);
---
+++ inflearn_c#/WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SigninButton(object sender, EventArgs e)
+        {
+            string userId = id.Text;
+            string userPassword = pw.Text;
+
+            if(userId.Equals("MyId") && userPassword.Equals("MyPassword"))
+            {
+                MessageBox.Show("로 그 인 성 공", "로그인");
+            } else
+            {
+                MessageBox.Show("로 그 인 실 패", "로그인");
+            }
+        }
+    }
+}
