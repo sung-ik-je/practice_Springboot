20296de2ca411aa08ecfe7dd28530c1187561d77
sung-ik-je <ij16_21@naver.com>
2024-03-05 20:02:31 +0900

    C에서 메모리를 할당, 해제 하는 이유

--- C_stringToOctetString/stringToOctetString.c
+++ C_stringToOctetString/stringToOctetString.c
@@ -8,7 +8,17 @@ unsigned char* stringToOctetString(const char* str) {
     // 문자열의 길이 계산
     size_t len = strlen(str);
 
-    // octet string을 저장할 배열 동적 할당
+    /*
+    octet string을 저장할 배열 동적 할당
+    
+    C에서 메모리를 동적할당하는 이유?
+      main -> stringToOctetString처럼 지역 변수는 함수가 호출될 때 메모리가 할당되며 종료될 때 해당 메모리가 해제된다(지역 변수는 함수 범위 내에서만 유효)
+      
+      메모리를 직접적으로 할당해주는 이유?
+        지역 변수는 전역 변수, 정적 지역 변수와는 다르게 프로그램이 시작될 때 자동으로 0 또는 NULL로 초기화되지 않는다
+          쓰레기 값이 할당되며 바로 사용하는 경우 다른 메모리 주소에 영향 줄 수 있다
+        때문에 동적으로 메모리 주소를 할당해준다
+    */
     unsigned char* octet_string = (unsigned char*)malloc(len * sizeof(unsigned char));
 
     // 문자열을 ASCII 값으로 변환하여 octet string에 저장
@@ -31,6 +41,11 @@ int main() {
     printf("\n");
 
     // octet string 메모리 해제
+    /*
+    main 함수 종료 시 octet_str 변수는 main 함수의 지역 변수이기에 함수가 종료되면 스택에서 제거되지만 
+    stringToOctetString 함수로부터 응답 받은 OctetString 형태가 저장된 메모리 부분(런타임 시 생성되었기에 힙에 존재)은 그대로 존재
+    때문에 main 함수 종료 전 free를 통해 사용성을 잃은 데이터를 삭제해주는 것
+    */
     free(octet_str);
 
     return 0;
