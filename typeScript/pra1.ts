6e5f1fa35a8d800f4e05e4a5ed686c8c8d60916f
sung-ik-je <ij16_21@naver.com>
2024-05-28 08:47:15 +0900

    del C code

---
+++ typeScript/pra1.ts
@@ -0,0 +1,13 @@
+const nico = {
+  name: "hello",
+  say : function say(a :string, b :number) {
+    console.log("say : ", a, b);
+  }
+}
+
+// nico.say('a', 3)
+
+let a:number;
+a = 1;
+console.log(typeof a);
+
